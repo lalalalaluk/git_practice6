ff3b64d3c8196fff24b2bd1b66920963df9012d6
luk <lalaluk5@ntub.edu.tw>
2023-03-11 11:06:50 +0800

    good function

--- test3.py
+++ test3.py
@@ -2,3 +2,11 @@ print("iiii")
 print("iiii")
 print("iiii")
 print("iiii")
+
+print('abcabcabcabc')
+print('abcabcabcabc')
+print('abcabcabcabc')
+print('abcabcabcabc')
+
+def test():
+    print("test")
